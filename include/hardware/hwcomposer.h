27e7435f823c2933c61442010678dd9a0f4db949
ST-Ericsson <void@stericsson.com>
2012-08-20 16:59:37 +0200

    Support for graphics hardware on Snowball for JB.
    Change-Id: I47ad6dd695636536520590ed6eb791fe9289ffd7
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/70899

--- include/hardware/hwcomposer.h
+++ include/hardware/hwcomposer.h
@@ -372,6 +372,20 @@ typedef struct hwc_module {
     struct hw_module_t common;
 } hwc_module_t;
 
+#ifdef TARGET_BOARD_SNOWBALL
+/*
+ * names for setParameter()
+ */
+enum {
+    /* Specifies the UI orientation */
+    HWC_UI_ORIENTATION = 0x00000000,
+    /* Specifies if hardware rotation is used */
+    HWC_HARDWARE_ROTATION = 0x00000001,
+    /* Set the hdmi plug status */
+    HWC_HDMI_PLUGGED = 0x00000002,
+};
+#endif
+
 typedef struct hwc_composer_device_1 {
     struct hw_device_t common;
 
@@ -561,6 +575,17 @@ typedef struct hwc_composer_device_1 {
      * Reserved for future use. Must be NULL.
      */
     void* reserved_proc[4];
+#ifdef TARGET_BOARD_SNOWBALL
+    /*
+     * This hook is vendor specific and optional.
+     *
+     * (*setParameter)() makes the hardware composer aware of the system state,
+     * e.g. hdmi plug status and ui rotation, so that it can make intelligent
+     * decisions on how to handle composed surfaces and cloning in the kernel.
+     */
+    int (*setParameter)(struct hwc_composer_device* dev,
+                int param, int value);
+#endif
 
 } hwc_composer_device_1_t;
 
